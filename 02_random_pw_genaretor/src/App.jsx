f84aa289ab6718acfbafe370450549e170e1e2d4
bng16 <biswajitnag161619@gmail.com>
2024-08-06 22:18:31 +0530

    done

--- 02_random_pw_genaretor/src/App.jsx
+++ 02_random_pw_genaretor/src/App.jsx
@@ -1,31 +1,99 @@
-import { useState, useCallback } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  //! useState
   const [length, setLength] = useState(8);
-  const [number, setNumber] = useState(false);
-  const [character, setCharecter] = useState(false);
-  const [pw, setPw] = useState("");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const pwGenerator=useCallback(()=>{
-    let pass='';
-    let str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    if (number) str+='0123456789';
-    if (character) str+='!@#$%^&*()_+{}|:"<>?~`-=[];,./';
+  //! useRef
+  const passRef = useRef(null);
 
-    for (let i=0; i<str.length;i++){
-      pass+=str.charAt(Math.floor(Math.random()*str.length-1));
+  //! useCallback
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()_+=-`~,./';[]{}:<>?";
+
+    for (let i = 0; i < length; i++) {
+      pass += str.charAt(Math.floor(Math.random() * str.length));
     }
-    setPw(pass);
 
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed]);
 
-  },[length,number,character,setPw])
+  //!  useEffect
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+
+  const copyPassToClipBoard=()=>{
+    passRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }
 
   return (
-    <>
-      <h1>Password Generator</h1>
-    </>
-  )
+    <div
+      id="background"
+      className="w-full h-screen bg-black flex justify-center items-center"
+    >
+      <div
+        id="pw-gen-container"
+        className="w-[500px] h-[200px] rounded-lg bg-slate-400 flex flex-col justify-around items-center"
+      >
+        <div id="displayArea" className=" ">
+          <input
+            type="text"
+            value={password}
+            ref={passRef}
+            readOnly
+            className="w-[250px] h-[35px] rounded-l-lg px-3"
+          />
+          <button className="w-[60px] h-[35px] rounded-r-lg bg-blue-500 text-white" onClick={copyPassToClipBoard}>
+            Copy
+          </button>
+        </div>
+
+        <div id="inputArea" className=" flex w-full justify-around">
+          <div
+            id="inputRangeArea"
+            className="flex flex-col justify-center items-center gap-2"
+          >
+            <input
+              type="range"
+              min={6}
+              max={50}
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label className="text-white">{`length : ${length}`}</label>
+          </div>
+
+          <div id="numberInputContainer">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => setNumberAllowed((prev) => !prev)}
+            />
+            <label className="ml-2">Number</label>
+          </div>
+
+          <div id="characterInputContainer">
+            <input
+              type="checkbox"
+              defaultChecked={characterAllowed}
+              onChange={() => setCharacterAllowed((prev) => !prev)}
+            />
+            <label className="ml-2">Character</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
