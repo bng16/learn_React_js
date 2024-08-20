ee7f35bac365241c85dcef307d373ed4dda778d7
bng16 <biswajitnag161619@gmail.com>
2024-08-20 22:20:42 +0530

    first gsap with react

--- 08_GSAP_with_React/src/App.jsx
+++ 08_GSAP_with_React/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 
 function App() {
+  useGSAP(()=>{
+    gsap.to('.box',{
+      x: 500,
+      y: 500,
+      duration: 1,
+      rotate: 720,
+    })
+  })
+
   return (
-    <>
-      
-    </>
+    <main className='w-full h-screen bg-black'>
+      <div className="box h-[100px] w-[100px] bg-red-500 rounded-lg"></div>
+    </main>
+
   )
 }
 
