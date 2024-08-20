c619ad994fab3b5bdc5b48f4c881bebb2d33e648
bng16 <biswajitnag161619@gmail.com>
2024-08-20 23:22:07 +0530

    animating box with button click

--- 08_GSAP_with_React/src/App.jsx
+++ 08_GSAP_with_React/src/App.jsx
@@ -1,21 +1,31 @@
-import { useRef } from 'react';
+import { useRef,useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
 
 function App() {
+  const [randNum,setRandNum] = useState(0)
+  const [randDegree,setRandDegree] = useState(0)
+  
+  
   useGSAP(()=>{
     gsap.to('.box',{
-      x: 500,
-      y: 500,
-      duration: 1,
-      rotate: 720,
+      x: randNum,
+      rotate: randDegree,
+      duration: 0.5,
     })
-  })
+  },[randNum,randDegree])
 
   return (
-    <main className='w-full h-screen bg-black'>
-      <div className="box h-[100px] w-[100px] bg-red-500 rounded-lg"></div>
+    <main className='w-full h-screen bg-black flex justify-center items-center flex-col gap-10'>
+      <button onClick={()=>
+        {
+          setRandNum(gsap.utils.random(-500, 500, 10))
+          setRandDegree(gsap.utils.random(-720, 720, 90))
+        }
+      } className='bg-white px-2 py-1 rounded-md font-semibold '>Aniamte</button>
+      <div className="box bg-red-300 w-[100px] h-[100px] rounded-md"></div>
+
     </main>
 
   )
